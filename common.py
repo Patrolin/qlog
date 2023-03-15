f87f99c93233bae4d2522e17fe3644517def0762
Patrolin <patrik@balas.cz>
2023-03-15 22:38:07 +0100

    sessionId

--- common.py
+++ common.py
@@ -5,3 +5,16 @@ def mkdir(path: str):
         _mkdir(path)
     except FileExistsError:
         pass
+
+NEXTID_PATH = "nextId.txt"
+
+def getNextId() -> int:
+    with open(NEXTID_PATH, "a+") as f:
+        pass
+    with open(NEXTID_PATH, "r") as f:
+        r = f.read()
+        print("ayaya", r)
+        id = int(r or "0")
+    with open(NEXTID_PATH, "w") as f:
+        f.write(str(id + 1))
+    return id
--- serve.py
+++ serve.py
@@ -6,19 +6,24 @@ from io import BufferedReader
 # https://security.stackexchange.com/questions/226095/pythons-http-server-library-basic-security-checks
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        if self.path.removeprefix("/") in ["", "index.html"]:
+        if self.path in ["/", "/index.html"]:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
             with open("index.html", "rb") as f:
                 self.wfile.write(f.read())
+        elif self.path == "/id":
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.end_headers()
+            self.wfile.write(str(getNextId()).encode())
         else:
             self.send_response(404)
 
     def do_POST(self):
         try:
             print(self.path)
-            length = int(self.headers.get('content-length') or "")
+            length = int(self.headers.get("content-length") or "0")
             rfile = cast(BufferedReader, self.rfile)
             read_bytes = rfile.read(length)
             read = read_bytes.decode("utf8")
